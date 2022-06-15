769b9b89164dac5e3867263e166b4ca99193b8a1
toanlebao <toan.lebao@halliburton.com>
2022-06-16 00:48:44 +0700

    use def home

--- app.py
+++ app.py
@@ -2,9 +2,15 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return '<h1>Hello World, my name is Le Bao Toan \
-            This is the Capstone project</h1>'
+# @app.route('/')
+# def index():
+#     return '<h1>Hello World, my name is Le Bao Toan \
+#             This is the Capstone project</h1>'
+
+@app.route("/")
+def home():
+    html = f"<h3>Hello World, my name is Le Bao Toan \
+            This is the Capstone project</h3>"
+    return html.format(format)
 
 app.run(host='localhost', port=80)
