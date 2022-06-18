a2ec9c1dead998e0a8a194510baba5476c29b208
toanlebao <toan.lebao@halliburton.com>
2022-06-18 10:42:12 +0700

    change docker image name

--- run_docker.sh
+++ run_docker.sh
@@ -4,10 +4,10 @@
 
 # Step 1:
 # Build image and add a descriptive tag
-docker build -t proj4:v1.0 .
+docker build -t proj5:v1.0 .
 # Step 2: 
 # List docker images
 docker images
 # Step 3: 
 # Run flask app
-docker run -p 80:80 proj4:v1.0
+docker run -p 80:80 proj5:v1.0
